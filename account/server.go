1c6c2e207f14576dbcfb37f7e5ae67924e822108
Deepak Kumar <139503599+whatDeepak@users.noreply.github.com>
2025-01-14 00:44:25 +0530

    Add account service with gRPC support

---
+++ account/server.go
@@ -0,0 +1,67 @@
+package account
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/whatDeepak/microservice-arch/account/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type grpcServer struct {
+	pb.UnimplementedAccountServiceServer
+	service Service
+}
+
+func ListenGRPC(s Service, port int) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return err
+	}
+	serv := grpc.NewServer()
+	pb.RegisterAccountServiceServer(serv, &grpcServer{
+		UnimplementedAccountServiceServer: pb.UnimplementedAccountServiceServer{},
+		service: s,
+	})
+	reflection.Register(serv)
+	return serv.Serve(lis)
+}
+
+func (s *grpcServer) PostAccount(ctx context.Context, r *pb.PostAccountRequest) (*pb.PostAccountResponse, error) {
+	a, err := s.service.PostAccount(ctx, r.Name)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.PostAccountResponse{Account: &pb.Account{
+		Id: a.ID,
+		Name: a.Name,
+	}}, nil
+}
+
+func (s *grpcServer) GetAccount(ctx context.Context, r *pb.GetAccountRequest) (*pb.GetAccountResponse, error) {
+	a, err := s.service.GetAccount(ctx, r.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.GetAccountResponse{Account: &pb.Account{
+		Id: a.ID,
+		Name: a.Name,
+	}}, nil
+}
+
+func (s *grpcServer) GetAccounts(ctx context.Context, r *pb.GetAccountsRequest) (*pb.GetAccountsResponse, error) {
+	a, err := s.service.GetAccounts(ctx, r.Skip, r.Take)
+	if err != nil {
+		return nil, err
+	}
+	accounts := []*pb.Account{}
+	for _, account := range a {
+		accounts = append(accounts, &pb.Account{
+			Id: account.ID,
+			Name: account.Name,
+		})
+	}
+	return &pb.GetAccountsResponse{Accounts: accounts}, nil
+}
--- go.mod
+++ go.mod
@@ -5,7 +5,10 @@ go 1.23.1
 require (
 	github.com/99designs/gqlgen v0.17.63
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/lib/pq v1.10.9
+	github.com/segmentio/ksuid v1.0.4
 	github.com/vektah/gqlparser/v2 v2.5.21
+	google.golang.org/grpc v1.69.4
 )
 
 require (
@@ -15,4 +18,10 @@ require (
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/sosodev/duration v1.3.1 // indirect
+	github.com/tinrab/retry v1.0.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53 // indirect
+	google.golang.org/protobuf v1.36.1 // indirect
 )
