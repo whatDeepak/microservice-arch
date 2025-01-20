78d7990cccaca780b2d8e19605fa0ee993a581d5
whatDeepak <deekay9357@gmail.com>
2025-01-21 03:31:51 +0530

    Implement resolvers

--- graphql/mutation_resolver.go
+++ graphql/mutation_resolver.go
@@ -4,19 +4,83 @@
 
 package main
 
-// import "context"
+import (
+	"context"
+	"errors"
+	"log"
+	"time"
+
+	"github.com/whatDeepak/microservice-arch/order"
+)
+
+var (
+	ErrInvalidParameter = errors.New("invalid parameter")
+)
 
 type mutationResolver struct {
 	server *Server
 }
 
-// func (r *mutationResolver) CreateAccount(ctx context.Context, in AccountInput) (*Account, error) {
-// } 
+// CreateAccount creates a new account
+func (r *mutationResolver) CreateAccount(ctx context.Context, in AccountInput) (*Account, error) {
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
 
-// func (r *mutationResolver) CreateProduct(ctx context.Context, in ProductInput) (*Product, error) {
+	a, err := r.server.accountClient.PostAccount(ctx, in.Name)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
 
-// }
+	return &Account{
+		ID:   a.ID,
+		Name: a.Name,
+	}, nil
+} 
 
-// func (r *mutationResolver) CreateOrder(ctx context.Context, in OrderInput) (*Order, error) {
+// CreateProduct creates a new product
+func (r *mutationResolver) CreateProduct(ctx context.Context, in ProductInput) (*Product, error) {
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
 
-// }
+	p, err := r.server.catalogClient.PostProduct(ctx, in.Name, in.Description, in.Price)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return &Product{
+		ID:          p.ID,
+		Name:        p.Name,
+		Description: p.Description,
+		Price:       p.Price,
+	}, nil
+}
+
+// CreateOrder creates a new order
+func (r *mutationResolver) CreateOrder(ctx context.Context, in OrderInput) (*Order, error) {
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	var products []order.OrderedProduct
+	for _, p := range in.Products {
+		if p.Quantity <= 0 {
+			return nil, ErrInvalidParameter
+		}
+		products = append(products, order.OrderedProduct{
+			ID:       p.ID,
+			Quantity: uint32(p.Quantity),
+		})
+	}
+	o, err := r.server.orderClient.PostOrder(ctx, in.AccountID, products)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return &Order{
+		ID:         o.ID,
+		CreatedAt:  o.CreatedAt,
+		TotalPrice: o.TotalPrice,
+	}, nil
+}
